f5c8818393b7c622d0243a99e79a9d4d909cb10c
hugosenari <hugosenari@gmail.com>
2022-05-12 11:49:52 -0300

    chore: re index avbin

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."avbin_nim-master".dir   = "master";
+  inputs."avbin_nim-master".owner = "nim-nix-pkgs";
+  inputs."avbin_nim-master".ref   = "master";
+  inputs."avbin_nim-master".repo  = "avbin_nim";
+  inputs."avbin_nim-master".type  = "github";
+  inputs."avbin_nim-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."avbin_nim-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
